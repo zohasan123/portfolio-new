e3e3a335ac94a36ac3d7eb0994fa50e7bea13400
zohasan123 <zohasan@ucsd.edu>
2025-01-31 15:08:30 -0800

    project number

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,8 @@
-console.log("projects.js loaded!");
-
-
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `Projects Count: ${projects.length}`;
+
 
